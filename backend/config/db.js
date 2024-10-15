3a2006232ae31533dd0ec842e3e0347544539560
Shvam15 <shivam.sngh01@gmail.com>
2024-10-16 01:52:53 +0530

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const connectDB = async () => {
-    mongoose.connection.on("connected", () => {
-        console.log("MongoDB Connected");
-    });
-    await mongoose.connect(`${process.env.MONGODB_URI}/ecommerce-store`)
+  mongoose.connection.on("connected", () => {
+    console.log("MongoDB Connected");
+  });
+  await mongoose.connect(`${process.env.MONGODB_URI}/ecommerce-store`);
+};
 
-}
+console.log(process.env.MONGODB_URI)
 
-export default connectDB
+export default connectDB;
