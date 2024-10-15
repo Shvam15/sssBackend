2e7c27b483ef3af1d25aabc8fc2f6c1fe24c433e
Shvam15 <shivam.sngh01@gmail.com>
2024-10-16 01:45:42 +0530

    asnuasd

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -11,7 +11,7 @@ const razorpayInstance = new razorpay({
     key_id: process.env.RAZORPAY_KEY_ID ||  "rzp_test_qeuXdVkQfgPPe3",
     key_secret: process.env.RAZORPAY_SECRET_KEY || "Eh21oX8hyVpVYsuIHwLy0cR4",
 })
-console.log(process.env.RAZORPAY_KEY_ID)
+// console.log(process.env.RAZORPAY_KEY_ID)
 console.log(process.env.RAZORPAY_SECRET_KEY)
 
 // placing orders using COD
